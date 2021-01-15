3e0d381b6c0aefea63281752d7f6c2841b0e37e0
Shirly <2823159643@qq.com>
2021-01-15 11:58:02 +0800

    2021/1/15

--- pages/cart/pay/pay.js
+++ pages/cart/pay/pay.js
@@ -1,66 +1,38 @@
 // pages/cart/pay/pay.js
+// import {getSetting,chooseAddress,openSetting} from "../../utils/asywx.js"
+// import {showModal,showToast} from "../../../utils/asywx.js"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    address:{},
+    cart:[],
+    totalPrice:0,
+    totalNum:0,
+  },
+  onShow(){
+     //获取缓存中的收货地址信息
+    const address =wx.getStorageSync('address');
+    //获取缓存中购物车数组
+    let cart = wx.getStorageSync('cart')||[];
+    //过滤后的购物车数组
+    cart = cart.filter(v=>v.check);
+
+    let totalPrice=0;
+    let totalNum=0;
+
+    cart.forEach(v=>{
+      if(v.check){
+        totalPrice += v.num * v.price;
+        totalNum += v.num ;
+      }
+    });
+  
+    this.setData({
+      cart,
+      totalPrice,
+      totalNum,
+      address,
+    });
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
