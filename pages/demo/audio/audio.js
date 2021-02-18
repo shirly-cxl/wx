bbe1547cb5f05340da82f99e88527f249aaa6ae7
Shirly <2823159643@qq.com>
2021-02-18 11:44:19 +0800

    更新

--- pages/demo/audio/audio.js
+++ pages/demo/audio/audio.js
@@ -7,7 +7,13 @@ Page({
   data: {
     src:'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3',
     play_staus:true,
+    present:0,
+    play_time:"00:00",
+    music_time:"00:00",
+    play_modules_index:0,
+    play_modules:["顺序","随机","单曲"]
   },
+  //播放/暂停
   onplay:function(){
     var play_staus = this.data.play_staus;
     if(play_staus){
@@ -22,7 +28,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // this.setData({
+    //   music_main:app.data.ge[options.id],
+    //   music_index:parseInt(options.id)
+    // })
   },
 
   /**
@@ -33,20 +42,64 @@ Page({
     this.audioCtx = wx.createAudioContext('myAudio');
     this.audioCtx.play();
   },
+  // 进度条
   timeupdate:function(e){
-    console.log(e);
+    //播放时长
+    var currentTime = e.detail.currentTime;
+    //总时长
+    var duration =  e.detail.duration;
+
+   this.setData({
+    present : Math.floor(currentTime / duration * 1000) / 10,
+    play_time:this.formtime(currentTime),
+    music_time:this.formtime(duration)
+   });
   },
   /**
-   * 生命周期函数--监听页面显示
+   * 切换
    */
-  onShow: function () {
+  changeMod: function () {
+   var index = this.data.play_modules_index;
+   if(index == 2){
+      index = 0;
+   }else{
+      index ++;
+   }
 
+   this.setData({
+     play_modules_index:index
+   })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  formtime:function(time){
+    var m = parseInt(time/60);
+    var s = parseInt(time) - m * 60 ;
+    m = m.toString();
+    s = s.toString();
+    m = m.length < 2 ? '0' + m : m;
+    s = s.length < 2 ? '0' + s : s;
+    return  m + ":" + s;
   },
+
+  next:function(){
+    var index;
+    var modul = this.data.play_modules[play_modules_index];
+    if(modul == "随机"){
+     index = parseInt(Math.random()* 10) 
+    }else{
+      var music_index = this.data.music_index;
+      if(music_index == 10-1){
+        music_index = 0;
+      }else{
+        index = music_index +1;
+      }
+    }
+
+    this.setData({
+      music_index :index,
+      play_staus:true,
+      music_main:app.data.ge[index]
+    })
+    this.audioCtx.play();
+  }
 })
